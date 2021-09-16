5ed25ef9b42d50c943c8d48a06960816b8fd1bb1
Josep Suarez <djose.anton@gmail.com>
2021-09-16 15:08:44 +0200

    Connect component header with redux

--- MyGithubApp/App.tsx
+++ MyGithubApp/App.tsx
@@ -1,21 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
+import {Provider} from 'react-redux';
+import store from './src/redux/store';
 import Header from './src/components/Header'
 import {
   StyleSheet,
 } from 'react-native';
 
-const App: () => Node = () => {
+const App= () => {
   return (
-    <Header />
+    <Provider store={store([])}>
+      <Header navigation={undefined} route={undefined} />
+    </Provider>
   );
 };
 
--- MyGithubApp/src/components/Header.tsx
+++ MyGithubApp/src/components/Header.tsx
@@ -1,16 +1,28 @@
 import React, {useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import {StackScreenProps} from '@react-navigation/stack';
 import {TouchableHighlight, View, Text, TextInput, SafeAreaView, ScrollView,  StyleSheet, Image} from 'react-native';
+import {getUsers} from '../redux/actions/actionCreators';
 
-const Header = () => {
+
+interface Props extends StackScreenProps<any, any> {}
+
+const Header = ({navigation}: Props) => {
+    const user = useSelector((store: any) => store.user);
+    const dispatch = useDispatch();
     let [userSearch, setUserSearch] = useState('');
+    function userSearched(user){
+        setUserSearch(user);
+        dispatch(getUsers(user))
+    }
     return (
         <View style={styles.headerContainer}>
             <TextInput
             style={styles.input}
             testID='searchInput'
-            placeholder="Que buscas?"
+            placeholder="Introduce el usuario"
             autoCapitalize="none"
-            onChangeText={(user: string) => {setUserSearch(user)}}
+            onChangeText={(user: string) => {userSearched(user)}}
             defaultValue={userSearch}
             value={userSearch}
             />
